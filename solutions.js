170b80ef6b308ff924c5a6b99384c608cfc15c60
= <liliasalas@lilias-imac.myfiosgateway.com>
2021-06-12 10:56:15 -0400

    part 1 and 2 done

--- solutions.js
+++ solutions.js
@@ -13,10 +13,21 @@ const sylvester = new Cat('Sylvester')
 console.log(garfield);
 console.log(sylvester);
 
+console.log(garfield.fight());
+console.log(sylvester.fight());
+
+const catFunction = function (cat) {
+  let newCat = cat
+}
+
 const arrayOfCats = []
-for(let i = 0; i < 5 ; i++) {
+for(let i = 0; i < 6 ; i++) {
   arrayOfCats.push(i);
   console.log(arrayOfCats);
 
 }
 console.log(arrayOfCats[3]);
+
+const catPerson = {
+cats: ['cat1', 'cat2', 'cat3']
+}
